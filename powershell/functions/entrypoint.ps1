81d4a1faca41a3fec23723ec4b18543b785ce481
Joan Mas <joan.mas@esblada.com>
2022-04-22 11:27:04 +0200

    move entrypoint to another file

---
+++ powershell/functions/entrypoint.ps1
@@ -0,0 +1,7 @@
+# env
+. $env:USERPROFILE\.config\powershell\env.ps1
+
+# Functions
+. $env:USERPROFILE\.config\powershell\functions\dockermachine.ps1
+. $env:USERPROFILE\.config\powershell\functions\gpconfig.ps1
+. $env:USERPROFILE\.config\powershell\functions\killspoty.ps1
--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -34,10 +34,6 @@ function which ($command) {
 
 function cdc { set-location $env:PROJ_PATH }
 
-# env
-. $env:USERPROFILE\.config\powershell\env.ps1
+# Function entrypoint file
+. $env:USERPROFILE\.config\powershell\functions\entrypoint.ps1
 
-# Functions
-. $env:USERPROFILE\.config\powershell\functions\dockermachine.ps1
-. $env:USERPROFILE\.config\powershell\functions\gpconfig.ps1
-. $env:USERPROFILE\.config\powershell\functions\killspoty.ps1
