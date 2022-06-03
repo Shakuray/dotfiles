31958bb7f7011f010333223bddac027e28d72d9f
Arnau Daza Requena <arnau.daza@esblada.com>
2022-06-03 11:07:11 +0200

    kill spotify restart process

--- powershell/functions/killspoty.ps1
+++ powershell/functions/killspoty.ps1
@@ -2,5 +2,6 @@ function killspoty() {
 	taskkill /IM 'Spotify.exe' /F
 
         $spotyIcon = [Char]0xf1bc
-        Write-Host "$spotyIcon Spotify successfully stopped" -ForegroundColor Green -NoNewline
+        Write-Host "$spotyIcon Spotify successfully restarted" -ForegroundColor Green -NoNewline
+	& $env:APPDATA\Spotify\Spotify.exe
 }
